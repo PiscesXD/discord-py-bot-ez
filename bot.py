83ffcc484dc9e882b0b89182471918ef6bf01e7e
PiscesXD <88549470+PiscesXD@users.noreply.github.com>
2022-07-19 02:56:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,19 @@ bot = commands.Bot(command_prefix="?" , intents = intents)
 @bot.event
 async def on_ready():
 	print("開啟成功~")
+	activity = discord.Game('Pisces') 
+	status = discord.Status.idle
+	await bot.change_presence(status=status , activity=activity)
 
+#指令觸發 ?嗨嗨
 @bot.commansd()
-async def hello(ctx):
-	return await ctx.send("你好呀")
+async def 嗨嗨(ctx):
+	return await ctx.send("嗨~")
+
+#文字觸發 早安
+@bot.event
+async def on_message(message):
+	if message.content == '早安':
+		await message.channel.send('早安ㄚ~')
 
 bot.run("bot token")
